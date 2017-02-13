eea128607a8e5897fc76b3c46ede7dee6ee98a5a
Benton <chan.benton+test@gmail.com>
2017-02-13 19:46:03 +0000

    Revised files to appropriately titled names for last step

--- finalIndex.js
+++
@@ -1,9 +0,0 @@
-var custom = require("./getHTML");
-var https = require('https');
-
-var reqOptions = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/step4.html'
-};
-
-custom.getHTML(reqOptions, custom.printHTML);
